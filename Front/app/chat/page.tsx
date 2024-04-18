f341de84fb0b83bea02bb769d9d8682023dbfe35
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-04-18 13:07:27 -0700

    First commit

---
+++ Front/app/chat/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Image from "next/image";
+import Profile from "../../public/profile.png";
+import Doctor from "../../public/doctor.png";
+import Link from "next/link";
+import { useState, useRef, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import Arrow from "../../public/arrow.png";
+
+export default function Home() {
+  const searchParams = useSearchParams();
+  const [input, setInput] = useState("");
+  const [id, setId] = useState(searchParams.get("id"));
+  const chatContainerRef = useRef(null);
+  const [message, setMessage] = useState([
+    { id: "2", message: "Hello Doctor!" },
+    { id: "1", message: "Hello! How are you?" },
+    { id: "2", message: "Good!" },
+  ]);
+
+  const createNewMessage = (e) => {
+    const newMessage = {
+      id: id,
+      message: input,
+    };
+    e.preventDefault();
+    setMessage([...message, newMessage]);
+    setInput("");
+  };
+
+  useEffect(() => {
+    chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+  }, [message]);
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gray-300 px-[40%]">
+      <div className="flex bg-blue-300 w-full py-2">
+        <Link href="/" className="flex flex-col justify-center ml-3">
+          <Image src={Arrow} width={30} alt="Picture of the author" />
+        </Link>
+        <div className="flex ml-5">
+          <Image
+            src={id === "1" ? Profile : Doctor}
+            width={30}
+            alt="Picture of the author"
+          />
+          <div className="flex flex-col justify-center ml-3">
+            <p className="text-xl">{id === "1" ? "Patient" : "Doctor"}</p>
+          </div>
+        </div>
+      </div>
+      <div
+        className="bg-white w-full h-[40rem] overscroll-y-auto overflow-y-auto"
+        ref={chatContainerRef}
+      >
+        <ul className="space-y-2">
+          {message.map((chat, index) => (
+            <li
+              key={index}
+              className={`p-3 ${id === chat.id ? "text-end" : "text-start"}`}
+            >
+              <p
+                className={`max-w-[50%] ${
+                  id === chat.id ? "message-sender" : "message"
+                }`}
+              >
+                {chat.message}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form className="w-full" onSubmit={createNewMessage}>
+        <input
+          type="text"
+          className="border w-4/5 p-3"
+          placeholder="Text here"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <button
+          type="submit"
+          className="w-1/5 p-3 bg-[#ffa74f] hover:bg-[#ffcd9c]"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ Front/app/page.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import Profile from "../public/profile.png";
+import Doctor from "../public/doctor.png";
+import Link from "next/link";
+
+type Person = {
+  id: string;
+  name: string;
+};
+
+export default function Home() {
+  const patient: Person = {
+    id: "1",
+    name: "Patient",
+  };
+  const doctor: Person = {
+    id: "2",
+    name: "Doctor",
+  };
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gray-300">
+      <h2 className="text-xl mb-10">Chat with...</h2>
+      <Link
+        href={{
+          pathname: "/chat",
+          query: {
+            id: doctor.id,
+          },
+        }}
+      >
+        <div className="flex bg-blue-400 hover:bg-blue-600 py-3 px-6 rounded mb-10">
+          <Image
+            src={Doctor}
+            width={50}
+            height={50}
+            alt="Picture of the author"
+          />
+          <div className="flex flex-col justify-center ml-5">
+            <p>{doctor.name}</p>
+          </div>
+        </div>
+      </Link>
+      <Link
+        href={{
+          pathname: "/chat",
+          query: {
+            id: patient.id,
+          },
+        }}
+      >
+        <div className="flex bg-green-400 hover:bg-green-600 py-3 px-6 rounded">
+          <Image
+            src={Profile}
+            width={50}
+            height={50}
+            alt="Picture of the author"
+          />
+          <div className="flex flex-col justify-center ml-5">
+            <p>{patient.name}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
