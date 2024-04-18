10a0492fd14959860c64fbd642baa50e6afffaec
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-04-18 14:13:51 -0700

    Set the suspense

--- Front/app/chat/page.tsx
+++ Front/app/chat/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { useState, useRef, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import Arrow from "../../public/arrow.png";
+import { Suspense } from "react";
 
 interface Message {
   id: string;
@@ -42,60 +43,62 @@ export default function Home() {
   }, [message]);
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-gray-300 px-[40%]">
-      <div className="flex bg-blue-300 w-full py-2">
-        <Link href="/" className="flex flex-col justify-center ml-3">
-          <Image src={Arrow} width={30} alt="Picture of the author" />
-        </Link>
-        <div className="flex ml-5">
-          <Image
-            src={id === "1" ? Profile : Doctor}
-            width={30}
-            alt="Picture of the author"
-          />
-          <div className="flex flex-col justify-center ml-3">
-            <p className="text-xl">{id === "1" ? "Patient" : "Doctor"}</p>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="h-screen flex flex-col justify-center items-center bg-gray-300 px-[40%]">
+        <div className="flex bg-blue-300 w-full py-2">
+          <Link href="/" className="flex flex-col justify-center ml-3">
+            <Image src={Arrow} width={30} alt="Picture of the author" />
+          </Link>
+          <div className="flex ml-5">
+            <Image
+              src={id === "1" ? Profile : Doctor}
+              width={30}
+              alt="Picture of the author"
+            />
+            <div className="flex flex-col justify-center ml-3">
+              <p className="text-xl">{id === "1" ? "Patient" : "Doctor"}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        className="bg-white w-full h-[40rem] overscroll-y-auto overflow-y-auto"
-        ref={chatContainerRef}
-      >
-        <ul className="space-y-2">
-          {message.map((chat, index) => (
-            <li
-              key={index}
-              className={`p-3 ${id === chat.id ? "text-end" : "text-start"}`}
-            >
-              <p
-                className={`max-w-[50%] ${
-                  id === chat.id ? "message-sender" : "message"
-                }`}
-              >
-                {chat.message}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <form className="w-full" onSubmit={createNewMessage}>
-        <input
-          type="text"
-          className="border w-4/5 p-3"
-          placeholder="Text here"
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
-        />
-        <button
-          type="submit"
-          className="w-1/5 p-3 bg-[#ffa74f] hover:bg-[#ffcd9c]"
+        <div
+          className="bg-white w-full h-[40rem] overscroll-y-auto overflow-y-auto"
+          ref={chatContainerRef}
         >
-          Send
-        </button>
-      </form>
-    </div>
+          <ul className="space-y-2">
+            {message.map((chat, index) => (
+              <li
+                key={index}
+                className={`p-3 ${id === chat.id ? "text-end" : "text-start"}`}
+              >
+                <p
+                  className={`max-w-[50%] ${
+                    id === chat.id ? "message-sender" : "message"
+                  }`}
+                >
+                  {chat.message}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <form className="w-full" onSubmit={createNewMessage}>
+          <input
+            type="text"
+            className="border w-4/5 p-3"
+            placeholder="Text here"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+          <button
+            type="submit"
+            className="w-1/5 p-3 bg-[#ffa74f] hover:bg-[#ffcd9c]"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </Suspense>
   );
 }
